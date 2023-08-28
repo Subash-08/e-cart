bee16ab6aed6a0cce4adb5d90c29a3a53bf0b4e3
Subash-08 <128483625+Subash-08@users.noreply.github.com>
2023-08-28 14:48:05 +0530

    initial code commit

---
+++ frontend/src/components/layouts/Footer.js
@@ -0,0 +1,9 @@
+export  default function Footer (){
+    return (
+        <footer >
+            <p className="text-center mt-1 footer md-0">
+                Cart - 2022-2023, All Rights Reserved
+            </p>
+        </footer>
+    )
+}
